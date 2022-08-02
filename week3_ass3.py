6361d7cba067eab64fd4fa8bf742de9a74dd12eb
kaphi10 <kafayatadenike10@gmail.com>
2022-08-02 12:02:59 +0100

    Added week3 assignment 3 to my folder

--- week3_ass3.py
+++ week3_ass3.py
@@ -1,30 +1,52 @@
 # Assignment class inheritance
 class Student:
         #declaring init method
-    def __init__(self, firstname,lastname,):
+    def __init__(self, firstname,lastname):
         self.firstname=firstname
         self.lastname=lastname
-    # declaring method to print
-    def display(self):
-        # print(self.name)
-        # print(self.id)
-        return '{} {} '. format(self.firstname,self.lastname)
-
+  
 class Group_lead(Student):
-    std=[]
-    other=[]
-    def __init__(self,firstname,lastname,std):
-        super().__init__(firstname, lastname)
-        
+
+    def __init__(self,firstname,lastname,std=[]):
         self.std=std
+        super().__init__(firstname, lastname)
+        # declaring method that add student to the list of student
+    def add_student(self,std):
+        self.std.append(std)
         
-        
-    def add_student(self):
-        self.other=self.std.append(self.other)
-        return self.other
-        
-    
-group1=Group_lead(firstname='Amina',lastname='Abubakar',std=['Aliyu Abdullahi','Kafayat Ibrahim'])
-print('The name of the Group1 Leader is:',group1.display() + 
-'The name of the student assigned to the group is',group1.std)
-print('I will like to add mor student to group1:',group1.add_student(list('aisha aina')))
+        print(f"I will like to add {self.std} to student's list")
+         # declaring method that add student to the list of student
+    def remove_student(self,std):
+        self.std.remove(std)
+        #print(self.std)
+        print(f" {self.std} is the student left after removing some students")
+          # declaring method to print full name of student
+    def display(self):
+        return '{} {} '. format(self.firstname,self.lastname)
+    def email(self):
+        self.mail=self.firstname+"."+self.lastname.lower()+"@stutern.com"
+        print(f"Email address of the student:{self.mail}")
+        #instance of student class
+group1=Group_lead("Amina","Lawal")
+group2=Group_lead("Mariam","Adeoti")
+group3=Group_lead("Joseph","Aroms")
+
+
+print('The name of the Group1 Leader is:',group1.display())
+group1.email()
+group1.add_student("Aliyu Ahmed")
+group1.add_student("Bashar Uthman")
+group1.remove_student("Aliyu Ahmed")
+print("The name of the group2 Leader is:",group2.display())
+group2.email()
+group2.add_student("Kafayat Ibrahim")
+group2.add_student("Temitope Bamidele")
+group2.remove_student("Kafayat Ibrahim")
+print("The name of the group2 Leader is:",group3.display())
+group3.email()
+group3.add_student("Adekunle Lawal")
+group3.add_student("Bidemi Ayeni")
+group3.remove_student("Bidemi Ayeni")
+
+
+
